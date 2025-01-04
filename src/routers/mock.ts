1c00ac8e372bd986bee3cb3ed83b7cd3229944be
DavidJones <qowera@gmail.com>
2025-01-05 01:10:29 +0800

    ci: remove vars

--- src/routers/mock.ts
+++ src/routers/mock.ts
@@ -5,14 +5,4 @@ import m3u from '@/controllers/m3u.mock'
 export default function iptv(router: RouterType) {
   router.get('/iptv.mock.m3u', m3u)
   router.get('/epg.mock.xml', epg)
-
-  router.get('/test', async () => {
-    const response = await fetch('https://epg.112114.xyz/pp.xml')
-    const content = await response.text()
-    return new Response(content, {
-      headers: {
-        'Content-Type': 'text/xml; charset=utf-8',
-      },
-    })
-  })
 }
